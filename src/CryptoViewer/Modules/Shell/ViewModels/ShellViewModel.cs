eda3812e5f382a1853ab5af4c6ea0db61b65aac0
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 03:32:40 +0300

    Export ShellViewModel as IShell. Implement IMenu

--- src/CryptoViewer/Modules/Shell/ViewModels/ShellViewModel.cs
+++ src/CryptoViewer/Modules/Shell/ViewModels/ShellViewModel.cs
@@ -1,16 +1,27 @@
 ﻿using Caliburn.Micro;
-using CryptoViewer.Services;
+using CryptoViewer.Base.Services;
+using CryptoViewer.Modules.MainMenu;
+using System.ComponentModel.Composition;
 
 namespace CryptoViewer.Modules.Shell.ViewModels
 {
+    [Export(typeof(IShell))]
     internal class ShellViewModel : Conductor<IScreen>.Collection.OneActive, IShell
     {
-        public ShellViewModel()
+        private IMenu _menu;
+        public IMenu MainMenu => _menu;
+
+        [ImportingConstructor]
+        public ShellViewModel(IMenu menu)
         {
+            _menu = menu;
         }
 
         public void ActivateItem(IScreen obj)
         {
+            if (ReferenceEquals(obj, this))
+                return;
+
             if (ReferenceEquals(obj, ActiveItem))
                 return;
 

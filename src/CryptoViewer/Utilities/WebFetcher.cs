28ab5e8f17e62bb87d943357190a91a86ff2c110
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 23:25:39 +0300

    Add webfetcher. Allows to send web request

---
+++ src/CryptoViewer/Utilities/WebFetcher.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Cache;
+using System.Text;
+using System.Threading;
+
+namespace SkinsTradeAssistant.Models
+{
+    internal class WebFetcher
+    {
+        public static string AcceptLanguageHeader { get { return acceptLanguageHeader; } set { acceptLanguageHeader = value; } }
+        private static string acceptLanguageHeader = Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName == "en" ? Thread.CurrentThread.CurrentCulture.ToString() + ",en;q=0.8" : Thread.CurrentThread.CurrentCulture.ToString() + "," + Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName + ";q=0.8,en;q=0.6";
+
+        public static CookieContainer _cookies = new CookieContainer();
+
+        /// <summary>
+        /// This method is using the Request method to return the full http stream from a web request as string.
+        /// </summary>
+        /// <param name="url">URL of the http request.</param>
+        /// <param name="method">Gets the HTTP data transfer method (such as GET, POST, or HEAD) used by the client.</param>
+        /// <param name="data">A NameValueCollection including Headers added to the request.</param>
+        /// <param name="ajax">A bool to define if the http request is an ajax request.</param>
+        /// <param name="referer">Gets information about the URL of the client's previous request that linked to the current URL.</param>
+        /// <param name="fetchError">If true, response codes other than HTTP 200 will still be returned, rather than throwing exceptions</param>
+        /// <returns>The string of the http return stream.</returns>
+        public static string Fetch(string url, string method, NameValueCollection data = null, bool ajax = true, string referer = "", bool fetchError = false, NameValueCollection headers = null)
+        {
+            // Reading the response as stream and read it to the end. After that happened return the result as string.
+            using (HttpWebResponse response = Request(url, method, data, ajax, referer, fetchError, headers))
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    // If the response stream is null it cannot be read. So return an empty string.
+                    if (responseStream == null)
+                    {
+                        return "";
+                    }
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        return reader.ReadToEnd();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Custom wrapper for creating a HttpWebRequest.
+        /// </summary>
+        /// <param name="url">Gets information about the URL of the current request.</param>
+        /// <param name="method">Gets the HTTP data transfer method (such as GET, POST, or HEAD) used by the client.</param>
+        /// <param name="data">A NameValueCollection including Headers added to the request.</param>
+        /// <param name="ajax">A bool to define if the http request is an ajax request.</param>
+        /// <param name="referer">Gets information about the URL of the client's previous request that linked to the current URL.</param>
+        /// <param name="fetchError">Return response even if its status code is not 200</param>
+        /// <returns>An instance of a HttpWebResponse object.</returns>
+        public static HttpWebResponse Request(string url, string method, NameValueCollection data = null, bool ajax = true, string referer = "", bool fetchError = false, NameValueCollection headers = null)
+        {
+            //Append the data to the URL for GET - requests.
+
+            bool isGetMethod = (method.ToLower() == "get");
+            string dataString = (data == null ? null : String.Join("&", Array.ConvertAll(data.AllKeys, key =>
+                string.Format("{0}={1}", WebUtility.UrlEncode(key), WebUtility.UrlEncode(data[key]))
+            )));
+
+            //Append the dataString to the url if it is a GET request.
+            if (isGetMethod && !string.IsNullOrEmpty(dataString))
+            {
+                url += (url.Contains("?") ? "&" : "?") + dataString;
+            }
+
+            //Setup the request.
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = method;
+            request.Accept = "application/json, text/javascript;q=0.9, */*;q=0.5";
+            request.Headers[HttpRequestHeader.AcceptLanguage] = AcceptLanguageHeader;
+            request.ContentType = "application/x-www-form-urlencoded; charset=UTF-8";
+            //request.Host is set automatically.
+            request.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36";
+            request.Referer = referer;
+            request.Timeout = 50000; // Timeout after 50 seconds.
+            request.CachePolicy = new HttpRequestCachePolicy(HttpRequestCacheLevel.Revalidate);
+            request.AutomaticDecompression = DecompressionMethods.Deflate | DecompressionMethods.GZip;
+            
+            if (headers != null)
+            {
+                request.PreAuthenticate = true;
+                request.Headers.Add(headers);
+            }
+
+            //If the request is an ajax request we need to add various other Headers, defined below.
+            if (ajax)
+            {
+                request.Headers.Add("X-Requested-With", "XMLHttpRequest");
+                request.Headers.Add("X-Prototype-Version", "1.7");
+            }
+
+            //Cookies
+            request.CookieContainer = _cookies;
+
+            //If the request is a GET request return now the response. If not go on. Because then we need to apply data to the request.
+            if (isGetMethod || string.IsNullOrEmpty(dataString))
+            {
+                return request.GetResponse() as HttpWebResponse;
+            }
+
+            //Write the data to the body for POST and other methods.
+            byte[] dataBytes = Encoding.UTF8.GetBytes(dataString);
+            request.ContentLength = dataBytes.Length;
+
+            using (Stream requestStream = request.GetRequestStream())
+            {
+                requestStream.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            //Get the response and return it.
+            try
+            {
+                return request.GetResponse() as HttpWebResponse;
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+                //this is thrown if response code is not 200
+                if (fetchError)
+                {
+                    var resp = ex.Response as HttpWebResponse;
+                    if (resp != null)
+                    {
+                        return resp;
+                    }
+                }
+                throw;
+            }
+        }
+    }
+}

f16828314ff8e73dab046af6040f333769f02f8f
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 03:29:27 +0300

    Add IMenu

---
+++ src/CryptoViewer/Modules/MainMenu/IMenu.cs
@@ -0,0 +1,11 @@
+﻿using CryptoViewer.Modules.MainMenu.Models;
+using System.Collections.Generic;
+
+namespace CryptoViewer.Modules.MainMenu
+{
+    internal interface IMenu
+    {
+        List<IMenuItem> Modules { get; }
+        IMenuItem SelectedContent { get; }
+    }
+}

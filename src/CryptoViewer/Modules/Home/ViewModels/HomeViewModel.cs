6ee258d3ba623d153713e0474cbf366f2a781021
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 23:36:29 +0300

    Add homeviewmodel and home view
    HomeViewModel is used to get data from api henlder

---
+++ src/CryptoViewer/Modules/Home/ViewModels/HomeViewModel.cs
@@ -0,0 +1,58 @@
+﻿using Caliburn.Micro;
+using CryptoViewer.Base.Interfaces;
+using CryptoViewer.Base.Services;
+using CryptoViewer.Handlers.Models;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Threading;
+
+namespace CryptoViewer.Modules.Home.ViewModels
+{
+    [Export(typeof(IHome))]
+    internal class HomeViewModel : Conductor<IScreen>, IHome
+    {
+        private IApiHandler _handler;
+
+        [ImportingConstructor]
+        public HomeViewModel(IApiHandler handler)
+        {
+            _handler = handler;
+        }
+
+        private IEnumerable<IExchanger> _exchangers;
+        public IEnumerable<IExchanger> Exchangers
+        {
+            get
+            {
+                if (_exchangers == null)
+                    _exchangers = _handler.GetExchangers();
+
+                return _exchangers;
+            }
+        }
+
+        private IExchanger _selectedExchanger;
+        public IExchanger SelectedExchanger
+        {
+            get { return _selectedExchanger; }
+            set
+            {
+                _selectedExchanger = value;
+                NotifyOfPropertyChange(nameof(Pairs));
+                NotifyOfPropertyChange(nameof(SelectedExchanger));
+            }
+        }
+
+        public IEnumerable<InnerPair> Pairs => _selectedExchanger != null ? _handler.GetCurrencies(_selectedExchanger) : Enumerable.Empty<InnerPair>();
+
+        protected override void OnViewLoaded(object view)
+        {
+            var exchanger = Exchangers.First();
+            if (exchanger == null)
+                return;
+
+            SelectedExchanger = exchanger;
+        }
+    }
+}
---
+++ src/CryptoViewer/Modules/Home/Views/HomeView.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CryptoViewer.Modules.Home.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для HomeView.xaml
+    /// </summary>
+    public partial class HomeView : UserControl
+    {
+        public HomeView()
+        {
+            InitializeComponent();
+        }
+
+        private void ComboBox_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void ComboBox_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+        }
+    }
+}

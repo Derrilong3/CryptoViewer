028fbb9e55f7ee1f9d7c158067890722bbb0398a
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 03:35:39 +0300

    Add MenuViewModel and MenuView

---
+++ src/CryptoViewer/Modules/MainMenu/ViewModels/MenuViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Caliburn.Micro;
+using CryptoViewer.Base.Services;
+using CryptoViewer.Modules.MainMenu.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Reflection;
+
+namespace CryptoViewer.Modules.MainMenu.ViewModels
+{
+    [Export(typeof(IMenu))]
+    internal class MenuViewModel : Conductor<IScreen>, IMenu
+    {
+        public List<IMenuItem> Modules { get; }
+
+        [ImportingConstructor]
+        public MenuViewModel()
+        {
+            Modules = Assembly.GetAssembly(typeof(IMenuItem)).GetTypes().Where(myType => !myType.IsAbstract && typeof(IMenuItem).IsAssignableFrom(myType)).Select(t => (IMenuItem)Activator.CreateInstance(t)).ToList();
+        }
+
+        private IMenuItem _selectedContent;
+        public IMenuItem SelectedContent
+        {
+            get => _selectedContent;
+            set
+            {
+                if (value != _selectedContent)
+                {
+                    _selectedContent = value;
+
+                    var shell = IoC.Get<IShell>();
+                    shell.ActivateItem(_selectedContent.UserInterface);
+                }
+            }
+        }
+
+        protected override void OnViewLoaded(object view)
+        {
+            //SelectedContent = Modules.FirstOrDefault(x => x.UserInterface.GetType() == typeof());
+        }
+    }
+}
---
+++ src/CryptoViewer/Modules/MainMenu/Views/MenuView.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoViewer.Modules.MainMenu.Views
+{
+    public partial class MenuView : UserControl
+    {
+        public MenuView()
+        {
+            InitializeComponent();
+        }
+    }
+}

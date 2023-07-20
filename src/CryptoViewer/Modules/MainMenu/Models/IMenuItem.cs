40c188baeaa364ee2450d2381eea22168a65b721
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 03:30:03 +0300

    Add IMenuItem

---
+++ src/CryptoViewer/Modules/MainMenu/Models/IMenuItem.cs
@@ -0,0 +1,10 @@
+﻿using Caliburn.Micro;
+
+namespace CryptoViewer.Modules.MainMenu.Models
+{
+    internal interface IMenuItem
+    {
+        string ImagePath { get; }
+        IScreen UserInterface { get; }
+    }
+}

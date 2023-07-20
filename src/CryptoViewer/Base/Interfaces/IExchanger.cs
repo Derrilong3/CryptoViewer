4eb4600e2329e9f3854b5a29c4cc01e26b3922ac
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 23:30:45 +0300

    Add base models which will be used in programm

---
+++ src/CryptoViewer/Base/Interfaces/IExchanger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoViewer.Base.Interfaces
+{
+    internal interface IExchanger
+    {
+        string Id { get; }
+        string Name { get; }
+        float? VolumeUsd { get; }
+    }
+}
---
+++ src/CryptoViewer/Base/Interfaces/IPair.cs
@@ -0,0 +1,14 @@
+﻿namespace CryptoViewer.Base.Interfaces
+{
+    internal interface IPair
+    {
+        string PairName { get; }
+        string BaseId { get; set; }
+        string BaseSymbol { get; set; }
+        string ExhangeId { get; set; }
+        float PriceUsd { get; set; }
+        string QuoteId { get; set; }
+        string QuoteSymbol { get; set; }
+        float VolumeUsd { get; set; }
+    }
+}

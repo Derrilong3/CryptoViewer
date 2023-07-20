b757a97d53904dbec97597f74fdbffcc37207e72
Derrilong3 <ivan2003.185@gmail.com>
2023-07-21 00:33:19 +0300

    Add attribute to mark properties that will be used in columns creation

---
+++ src/CryptoViewer/Utilities/Attributes/ItemDisplayDataAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CryptoViewer.Utilities.Attributes
+{
+    [AttributeUsage(AttributeTargets.Property)]
+    internal class ItemColumnDataAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public int Width { get; set; }
+        public string Group { get; set; }
+
+        public ItemColumnDataAttribute() { }
+
+        public ItemColumnDataAttribute(string name, int width = 100, string group = "")
+        {
+            Name = name;
+            Width = width;
+            Group = group;
+        }
+    }
+}

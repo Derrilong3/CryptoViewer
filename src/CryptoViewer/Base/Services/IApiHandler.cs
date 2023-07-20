fba3974deea8bbae25615b28af3d2b446689189a
Derrilong3 <ivan2003.185@gmail.com>
2023-07-20 23:31:40 +0300

    Add new api handler service

---
+++ src/CryptoViewer/Base/Services/IApiHandler.cs
@@ -0,0 +1,14 @@
+﻿using CryptoViewer.Base.Interfaces;
+using CryptoViewer.Handlers.Models;
+using System.Collections.Generic;
+
+namespace CryptoViewer.Base.Services
+{
+    internal interface IApiHandler
+    {
+        IEnumerable<IExchanger> GetExchangers();
+        IEnumerable<ICurrency> GetExchangers(ICurrency exchanger);
+        IEnumerable<ICurrency> GetCurrencies();
+        IEnumerable<InnerPair> GetCurrencies(IExchanger exchanger);
+    }
+}

bbf8db36e8e5eb8a6014104a88ca23f77debf4e6
Derrilong3 <ivan2003.185@gmail.com>
2023-07-21 03:31:01 +0300

    Add ICoin interface

--- src/CryptoViewer/Base/Interfaces/ICoin.cs
+++ src/CryptoViewer/Base/Interfaces/ICoin.cs
@@ -2,5 +2,14 @@
 {
     internal interface ICoin
     {
+        string Id { get; set; }
+        string Name { get; set; }
+        int Rank { get; set; }
+        string Symbol { get; set; }
+        float MarketCapUsd { get; set; }
+        float Supply { get; set; }
+        float VolumeUsd { get; set; }
+        float ChangePercent { get; set; }
+        float VWAP { get; set; }
     }
 }

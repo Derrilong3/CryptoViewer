663015a5fe07da163c3911d73f7cf3a8067aa7c2
Derrilong3 <ivan2003.185@gmail.com>
2023-07-23 00:39:35 +0300

    Fix lack of price property

--- src/CryptoViewer/Base/Interfaces/ICoin.cs
+++ src/CryptoViewer/Base/Interfaces/ICoin.cs
@@ -5,6 +5,7 @@
         string Id { get; set; }
         string Name { get; set; }
         int Rank { get; set; }
+        float Price { get; set; }
         string Symbol { get; set; }
         float MarketCapUsd { get; set; }
         float Supply { get; set; }

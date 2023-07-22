b038ba4a99c9c53a6392ee2545a7b61ddbc77161
Derrilong3 <ivan2003.185@gmail.com>
2023-07-22 19:38:47 +0300

    Override Exchange id

--- src/CryptoViewer/Handlers/Models/PairByCurrency.cs
+++ src/CryptoViewer/Handlers/Models/PairByCurrency.cs
@@ -1,6 +1,12 @@
-﻿namespace CryptoViewer.Handlers.Models
+﻿using CryptoViewer.Utilities.Attributes;
+using Newtonsoft.Json;
+
+namespace CryptoViewer.Handlers.Models
 {
     internal class PairByCurrency : Pair
     {
+        [ItemColumnData("Exchange")]
+        [JsonProperty("exchangeId")]
+        public override string ExhangeId { get; set; }
     }
 }

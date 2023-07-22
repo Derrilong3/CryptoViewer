f5a47e6395ab34787fd40284fe9fbf575c32c21c
Derrilong3 <ivan2003.185@gmail.com>
2023-07-22 16:59:35 +0300

    Add IWebFetcher

---
+++ src/CryptoViewer/Base/Services/IWebFetcher.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Specialized;
+using System.Net;
+
+namespace CryptoViewer.Base.Services
+{
+    internal interface IWebFetcher
+    {
+        CookieContainer Cookies { get; }
+
+        string Fetch(string url, string method, NameValueCollection data = null, bool ajax = true, string referer = "", bool fetchError = false, NameValueCollection headers = null);
+        HttpWebResponse Request(string url, string method, NameValueCollection data = null, bool ajax = true, string referer = "", bool fetchError = false, NameValueCollection headers = null);
+    }
+}

551b4e6d36f44263f0c8aadc2c83f12fee0393db
Derrilong3 <ivan2003.185@gmail.com>
2023-07-22 19:42:24 +0300

    Add listview with info that points where currency can be purchased

--- src/CryptoViewer/Modules/CoinDetails/ViewModels/DetailsViewModel.cs
+++ src/CryptoViewer/Modules/CoinDetails/ViewModels/DetailsViewModel.cs
@@ -2,10 +2,14 @@
 using CryptoViewer.Base.Interfaces;
 using CryptoViewer.Base.Services;
 using CryptoViewer.Charts.LiveCharts;
+using CryptoViewer.Utilities.GridViewUtilities;
 using LiveCharts;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Data;
 
 namespace CryptoViewer.Modules.CoinDetails.ViewModels
 {
@@ -14,23 +18,65 @@ namespace CryptoViewer.Modules.CoinDetails.ViewModels
     {
         private IApiHandler _apiHandler;
 
-        public ICoin Coin { get; set; }
-
         [ImportingConstructor]
         public DetailsViewModel(IApiHandler apiHandler)
         {
             _apiHandler = apiHandler;
             _chart = new LiveChartsModel();
+            _gridHandler = new GridViewHandler();
         }
+
+        private GridViewHandler _gridHandler;
+        public GridViewHandler GridView => _gridHandler;
 
         private LiveChartsModel _chart;
         public SeriesCollection Chart => _chart.SeriesCollection;
         public string[] Labels => _chart.Labels;
 
+        private ICoin _coin;
+        public ICoin Coin
+        {
+            get => _coin;
+            set
+            {
+                if (value == null || _coin == value)
+                    return;
+
+                _coin = value;
+
+                Pairs = _apiHandler.GetExchangers(_coin);
+
+                NotifyOfPropertyChange(nameof(Coin));
+            }
+        }
+
+        private IEnumerable<IPair> _pairs;
+        public IEnumerable<IPair> Pairs
+        {
+            get => _pairs;
+            set
+            {
+                _pairs = value;
+
+                if (_gridHandler.View == null && _pairs.Count() > 0)
+                {
+                    _gridHandler.CreateGridColumns((string.Empty, _pairs.First().GetType()));
+                }
+
+                _gridHandler.View = (CollectionView)CollectionViewSource.GetDefaultView(_pairs);
+
+                NotifyOfPropertyChange(nameof(Pairs));
+            }
+        }
+
         public void IntervalValue(string interval)
         {
+            if (Coin == null)
+                return;
+
             double[][] data = _apiHandler.GetOHLC(Coin, interval);
             _chart.InitCandleStickChart(Coin.Name, data);
+
             NotifyOfPropertyChange(nameof(Labels));
         }
 

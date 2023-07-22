550ded1720ce83f2dfc34f29d739422f5e879c6e
Derrilong3 <ivan2003.185@gmail.com>
2023-07-22 16:59:11 +0300

    Add container for candles data
    Change interval type from int to string
    Inject IWebFetcher instead of using static calls

--- src/CryptoViewer/Handlers/CoinCap/CoinCapHandler.cs
+++ src/CryptoViewer/Handlers/CoinCap/CoinCapHandler.cs
@@ -21,18 +21,23 @@ namespace CryptoViewer.Handlers.CoinCap
         private const string GetPairsByMarketUrl = "https://api.coincap.io/v2/markets";
         private const string GetCoinsDatatUrl = "https://api.coincap.io/v2/assets";
 
-        private IEnumerable<ICoin> _coinGeckos;
+        private IWebFetcher _webFetcher;
 
-        public CoinCapHandler()
+        private IEnumerable<ICoin> _coinGeckos;
+        private Dictionary<string, Dictionary<string, double[][]>> _candles;
+
+        [ImportingConstructor]
+        public CoinCapHandler(IWebFetcher webFetcher)
         {
-            string s = WebFetcher.Fetch("https://api.coincap.io/v2/candles?exchange=binance&interval=h8&baseId=BTC&quoteId=USD", "GET");
+            _candles = new Dictionary<string, Dictionary<string, double[][]>>();
+            _webFetcher = webFetcher;
         }
 
         public IEnumerable<IExchanger> GetExchangers()
         {
             try
             {
-                string rawJson = WebFetcher.Fetch(GetExchangersUrl, "GET");
+                string rawJson = _webFetcher.Fetch(GetExchangersUrl, "GET");
 
                 Root<Exchanger> root = JsonConvert.DeserializeObject<Root<Exchanger>>(rawJson);
 
@@ -67,7 +72,7 @@ namespace CryptoViewer.Handlers.CoinCap
                     { "limit", "2000" }
                 };
 
-                string rawJson = WebFetcher.Fetch(GetCoinsDatatUrl, "GET", data);
+                string rawJson = _webFetcher.Fetch(GetCoinsDatatUrl, "GET", data);
 
                 Root<Coin> root = JsonConvert.DeserializeObject<Root<Coin>>(rawJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
@@ -89,7 +94,7 @@ namespace CryptoViewer.Handlers.CoinCap
                     { "limit", 30.ToString() }
                 };
 
-                string rawJson = WebFetcher.Fetch(GetPairsByMarketUrl, "GET", data);
+                string rawJson = _webFetcher.Fetch(GetPairsByMarketUrl, "GET", data);
 
                 Root<InnerPair> root = JsonConvert.DeserializeObject<Root<InnerPair>>(rawJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 
@@ -102,7 +107,7 @@ namespace CryptoViewer.Handlers.CoinCap
             }
         }
 
-        public double[][] GetOHLC(ICoin coin, int interval)
+        public double[][] GetOHLC(ICoin coin, string interval)
         {
             try
             {
@@ -111,17 +116,36 @@ namespace CryptoViewer.Handlers.CoinCap
 
                 string id = _coinGeckos.First(x => x.Symbol.ToLower() == coin.Symbol.ToLower()).Id;
 
+                if (_candles.TryGetValue(id, out var intervals) && intervals.ContainsKey(interval))
+                {
+                    return intervals[interval];
+                }
+
                 string getOHCLUrl = $"https://api.coingecko.com/api/v3/coins/{id}/ohlc";
 
                 NameValueCollection data = new NameValueCollection
                 {
                     { "vs_currency", "usd" },
-                    { "days", interval.ToString() }
+                    { "days", interval }
                 };
 
-                string rawJson = WebFetcher.Fetch(getOHCLUrl, "GET", data);
+                string rawJson = _webFetcher.Fetch(getOHCLUrl, "GET", data);
 
                 double[][] result = JsonConvert.DeserializeObject<double[][]>(rawJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
+
+                if (_candles.TryGetValue(id, out intervals))
+                {
+                    intervals.Add(interval, result);
+                }
+                else
+                {
+                    intervals = new Dictionary<string, double[][]>()
+                    {
+                        { interval, result }
+                    };
+
+                    _candles.Add(id, intervals);
+                }
 
                 return result;
             }
@@ -136,7 +160,7 @@ namespace CryptoViewer.Handlers.CoinCap
         {
             string getCoinList = "https://api.coingecko.com/api/v3/coins/list";
 
-            string rawJson = WebFetcher.Fetch(getCoinList, "GET");
+            string rawJson = _webFetcher.Fetch(getCoinList, "GET");
 
             Coin[] coins = JsonConvert.DeserializeObject<Coin[]>(rawJson, new JsonSerializerSettings { NullValueHandling = NullValueHandling.Ignore });
 

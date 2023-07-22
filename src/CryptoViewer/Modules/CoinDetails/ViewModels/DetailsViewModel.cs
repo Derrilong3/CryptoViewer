6f67e47c54576eed7fbcb9c5c8527d7b1852b7a5
Derrilong3 <ivan2003.185@gmail.com>
2023-07-22 03:18:15 +0300

    Add Details model. Shows details of chosen coin

---
+++ src/CryptoViewer/Modules/CoinDetails/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Caliburn.Micro;
+using CryptoViewer.Base.Interfaces;
+using CryptoViewer.Base.Services;
+using CryptoViewer.Charts.LiveCharts;
+using LiveCharts;
+using System.ComponentModel.Composition;
+
+namespace CryptoViewer.Modules.CoinDetails.ViewModels
+{
+    [Export(typeof(IDetails))]
+    internal class DetailsViewModel : Conductor<IScreen>, IDetails
+    {
+        private IApiHandler _apiHandler;
+
+        public ICoin Coin { get; set; }
+
+        [ImportingConstructor]
+        public DetailsViewModel(IApiHandler apiHandler)
+        {
+            _apiHandler = apiHandler;
+            _chart = new LiveChartsModel();
+        }
+
+        private LiveChartsModel _chart;
+        public SeriesCollection Chart => _chart.SeriesCollection;
+        public string[] Labels => _chart.Labels;
+
+        protected override void OnViewReady(object view)
+        {
+            double[][] data = _apiHandler.GetOHLC(Coin, 30);
+            _chart.InitCandleStickChart(data);
+            NotifyOfPropertyChange(nameof(Labels));
+        }
+    }
+}
---
+++ src/CryptoViewer/Modules/CoinDetails/Views/DetailsView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace CryptoViewer.Modules.CoinDetails.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для DetailsView.xaml
+    /// </summary>
+    public partial class DetailsView : UserControl
+    {
+        public DetailsView()
+        {
+            InitializeComponent();
+        }
+    }
+}

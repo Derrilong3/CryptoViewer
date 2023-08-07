c84928333c662ff0a0e82b9f48de96a4d85cf4e0
Derrilong3 <ivan2003.185@gmail.com>
2023-08-08 02:04:31 +0300

    Add RestClient service

---
+++ src/CryptoViewer/Base/Services/IRestClient.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Specialized;
+using System.Threading.Tasks;
+
+namespace CryptoViewer.Base.Services
+{
+    internal interface IRestClient
+    {
+        Task<T> GetAsync<T>(string url, NameValueCollection query = null, NameValueCollection headers = null);
+        Task<string> GetAsync(string url, NameValueCollection query = null, NameValueCollection headers = null);
+        Task<T> PostAsync<T>(string url, NameValueCollection data = null, NameValueCollection headers = null);
+        Task<string> PostAsync(string url, NameValueCollection data = null, NameValueCollection headers = null);
+        Task<R> PostAsJsonAsync<T, R>(string url, T data, NameValueCollection headers = null);
+        Task<string> PostAsJsonAsync<T>(string url, T data, NameValueCollection headers = null);
+    }
+}

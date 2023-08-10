b45ea1ac05218440261aedd796b77cd4b79852c3
Derrilong3 <ivan2003.185@gmail.com>
2023-08-10 20:27:14 +0300

    Rename R generic type on TR

--- src/CryptoViewer/Base/Services/IRestClient.cs
+++ src/CryptoViewer/Base/Services/IRestClient.cs
@@ -9,7 +9,7 @@ namespace CryptoViewer.Base.Services
         Task<string> GetAsync(string url, NameValueCollection query = null, NameValueCollection headers = null);
         Task<T> PostAsync<T>(string url, NameValueCollection data = null, NameValueCollection headers = null);
         Task<string> PostAsync(string url, NameValueCollection data = null, NameValueCollection headers = null);
-        Task<R> PostAsJsonAsync<T, R>(string url, T data, NameValueCollection headers = null);
+        Task<TR> PostAsJsonAsync<T, TR>(string url, T data, NameValueCollection headers = null);
         Task<string> PostAsJsonAsync<T>(string url, T data, NameValueCollection headers = null);
     }
 }

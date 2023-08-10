592df06218e96bf006f06a97ab3b1937d954c41f
Derrilong3 <ivan2003.185@gmail.com>
2023-08-10 19:39:44 +0300

    Initialize properties when view is loaded. Small fixes.
    Simplify getters.
    Inherit Screen instead of Conductor<Screen>.
    Make _apiHandler and _chart readonly.
    Add Async suffix to async methods.
    Remove redundant column.
    Chnage size of buttons.

--- src/CryptoViewer/Modules/CoinDetails/ViewModels/DetailsViewModel.cs
+++ src/CryptoViewer/Modules/CoinDetails/ViewModels/DetailsViewModel.cs
@@ -14,25 +14,22 @@ using System.Windows.Data;
 namespace CryptoViewer.Modules.CoinDetails.ViewModels
 {
     [Export(typeof(IDetails))]
-    internal class DetailsViewModel : Conductor<IScreen>, IDetails
+    internal class DetailsViewModel : Screen, IDetails
     {
-        private IApiHandler _apiHandler;
+        private readonly IApiHandler _apiHandler;
+        private readonly LiveChartsModel _chart;
 
         [ImportingConstructor]
         public DetailsViewModel(IApiHandler apiHandler)
         {
             _apiHandler = apiHandler;
             _chart = new LiveChartsModel();
-            _gridHandler = new GridViewHandler();
+            GridView = new GridViewHandler();
         }
 
-        private GridViewHandler _gridHandler;
-        public GridViewHandler GridView => _gridHandler;
-
-        private LiveChartsModel _chart;
+        public GridViewHandler GridView { get; }
         public SeriesCollection Chart => _chart.SeriesCollection;
         public string[] Labels => _chart.Labels;
-
         public string Name => $"{Coin.Name} ({Coin.Symbol})";
 
         private ICoin _coin;
@@ -46,7 +43,7 @@ namespace CryptoViewer.Modules.CoinDetails.ViewModels
 
                 _coin = value;
 
-                GetPairs();
+                SetPairsAsync();
 
                 NotifyOfPropertyChange(nameof(Coin));
                 NotifyOfPropertyChange(nameof(Name));
@@ -56,13 +53,7 @@ namespace CryptoViewer.Modules.CoinDetails.ViewModels
         private IEnumerable<IPair> _pairs;
         public IEnumerable<IPair> Pairs
         {
-            get
-            {
-                if (_pairs == null)
-                    return Enumerable.Empty<IPair>();
-
-                return _pairs;
-            }
+            get => _pairs ?? Enumerable.Empty<IPair>();
             private set
             {
                 _pairs = value;
@@ -71,24 +62,24 @@ namespace CryptoViewer.Modules.CoinDetails.ViewModels
             }
         }
 
-        private async Task GetPairs()
+        private async Task SetPairsAsync()
         {
             Pairs = await _apiHandler.GetExchangers(_coin);
 
-            if (_gridHandler.Columns == null && _pairs.Count() > 0)
+            if (GridView.Columns == null && _pairs.Any())
             {
-                _gridHandler.CreateGridColumns((string.Empty, _pairs.First().GetType()));
+                GridView.CreateGridColumns((string.Empty, _pairs.First().GetType()));
             }
 
-            _gridHandler.View = (CollectionView)CollectionViewSource.GetDefaultView(_pairs);
+            GridView.View = (CollectionView)CollectionViewSource.GetDefaultView(_pairs);
         }
 
-        public async Task IntervalValue(string interval)
+        public async Task IntervalValueAsync(string interval)
         {
             if (Coin == null)
                 return;
 
-            double[][] data = await _apiHandler.GetOHLC(Coin, interval);
+            var data = await _apiHandler.GetOHLC(Coin, interval);
             _chart.InitCandleStickChart(Coin.Name, data);
 
             NotifyOfPropertyChange(nameof(Labels));
@@ -96,7 +87,7 @@ namespace CryptoViewer.Modules.CoinDetails.ViewModels
 
         protected override async void OnViewReady(object view)
         {
-            await IntervalValue("1");
+            await IntervalValueAsync("1");
         }
 
         protected override Task OnDeactivateAsync(bool close, CancellationToken cancellationToken)

556d34c590794e249276a10a703e62bfdc6af20d
Derrilong3 <ivan2003.185@gmail.com>
2023-08-16 17:45:13 +0300

    Load modules by importing instead of using reflection
    Import IShell.
    Change IMenuItem on IModule.

--- src/CryptoViewer/Modules/MainMenu/ViewModels/MenuViewModel.cs
+++ src/CryptoViewer/Modules/MainMenu/ViewModels/MenuViewModel.cs
@@ -1,39 +1,32 @@
 ﻿using Caliburn.Micro;
+using CryptoViewer.Base;
 using CryptoViewer.Base.Services;
 using CryptoViewer.Modules.Home.ViewModels;
-using CryptoViewer.Modules.MainMenu.Models;
-using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
-using System.Reflection;
 
 namespace CryptoViewer.Modules.MainMenu.ViewModels
 {
     [Export(typeof(IMenu))]
     internal class MenuViewModel : Conductor<IScreen>, IMenu
     {
-        public List<IMenuItem> Modules { get; }
+        [Import]
+        private IShell _shell;
 
-        public MenuViewModel()
-        {
-            Modules = Assembly.GetAssembly(typeof(IMenuItem))
-                .GetTypes()
-                .Where(myType => !myType.IsAbstract && typeof(IMenuItem)
-                .IsAssignableFrom(myType))
-                .Select(t => (IMenuItem)Activator.CreateInstance(t))
-                .ToList();
-        }
+        [ImportMany]
+        private List<IModule> _modules;
 
-        public void ClickMenu(IMenuItem item)
+        public List<IModule> Modules => _modules;
+
+        public void ClickMenu(IModule item)
         {
-            var shell = IoC.Get<IShell>();
             var selectedContent = item.UserInterface;
 
-            if (selectedContent == shell.ActiveItem)
+            if (selectedContent == _shell.ActiveItem)
                 return;
 
-            shell.ActivateItem(selectedContent);
+            _shell.ActivateItem(selectedContent);
         }
 
         protected override void OnViewLoaded(object view)
